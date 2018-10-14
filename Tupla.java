45b33e111d66905ddaefe9389109489116b8fde9
fcxS <nachher@hotmail.com>
2018-10-14 19:11:37 -0300

    tupleano

--- Tupla.java
+++ Tupla.java
@@ -1,5 +1,30 @@
 package uno;
 
-public class Tupla {
+public class Tupla<T1, T2> {
+	T1 e1;
+	T2 e2;
 
+	public Tupla(T1 e1, T2 e2) {
+		this.e1 = e1;
+		this.e2 = e2;
+	}
+
+	@Override
+	public boolean equals(Object t2) {
+		boolean ret = true;
+		if (!(t2 instanceof Tupla))
+			ret = false;
+		else {
+			// es importante no asumir <T1,T2> para t2,pues podria ser <T3,T4>
+			Tupla<T1, T2> t3 = (Tupla) t2;
+			ret = ret && t3.e1.equals(e1) && t3.e2.equals(e2);
+		}
+		return ret;
+	}
+
+	@Override
+	public String toString() {
+		String ret = e1.toString() + "," + e2.toString() + ";";
+		return ret;
+	}
 }
